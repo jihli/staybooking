59dade00128c191c9d2651d5ed61f0de93dbad0d
jihaoli <a276901052@outlook.com>
2024-12-02 15:27:14 -0800

    1202  - Google Cloud Platform Setup

--- src/main/java/com/laioffer/staybooking/AppConfig.java
+++ src/main/java/com/laioffer/staybooking/AppConfig.java
@@ -1,11 +1,20 @@
 package com.laioffer.staybooking;
 
 
+import com.google.auth.Credentials;
+import com.google.auth.oauth2.ServiceAccountCredentials;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+import com.google.maps.GeoApiContext;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+
+
+import java.io.IOException;
 
 
 @Configuration
@@ -24,5 +33,18 @@ public class AppConfig {
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         return http.build();
+    }
+
+
+    @Bean
+    public Storage storage() throws IOException {
+        Credentials credentials = ServiceAccountCredentials.fromStream(getClass().getClassLoader().getResourceAsStream("credentials.json"));
+        return StorageOptions.newBuilder().setCredentials(credentials).build().getService();
+    }
+
+
+    @Bean
+    public GeoApiContext geoApiContext(@Value("${staybooking.geocoding.key}") String apiKey) {
+        return new GeoApiContext.Builder().apiKey(apiKey).build();
     }
 }
