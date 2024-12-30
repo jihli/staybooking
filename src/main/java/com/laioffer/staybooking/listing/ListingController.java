d12f6b2b4414d67541927b14074dd71f3f50c743
jihaoli <a276901052@outlook.com>
2024-12-30 15:46:01 -0800

    12/30 - Authentication (Create project.toml)

--- src/main/java/com/laioffer/staybooking/listing/ListingController.java
+++ src/main/java/com/laioffer/staybooking/listing/ListingController.java
@@ -7,6 +7,7 @@ import com.laioffer.staybooking.model.ListingDto;
 import com.laioffer.staybooking.model.UserEntity;
 import com.laioffer.staybooking.model.UserRole;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -24,9 +25,6 @@ public class ListingController {
     private final ListingService listingService;
 
 
-    private final UserEntity user = new UserEntity(1L, "rich_the_landlord", "YT61cW", UserRole.ROLE_HOST);
-
-
     public ListingController(BookingService bookingService, ListingService listingService) {
         this.bookingService = bookingService;
         this.listingService = listingService;
@@ -34,7 +32,7 @@ public class ListingController {
 
 
     @GetMapping
-    public List<ListingDto> getListings() {
+    public List<ListingDto> getListings(@AuthenticationPrincipal UserEntity user) {
         return listingService.getListings(user.getId());
     }
 
@@ -42,6 +40,7 @@ public class ListingController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void createListing(
+            @AuthenticationPrincipal UserEntity user,
             @RequestParam("name") String name,
             @RequestParam("address") String address,
             @RequestParam("description") String description,
@@ -54,7 +53,7 @@ public class ListingController {
 
     @DeleteMapping("/{listingId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteListing(@PathVariable Long listingId) {
+    public void deleteListing(@AuthenticationPrincipal UserEntity user, @PathVariable Long listingId) {
         listingService.deleteListing(user.getId(), listingId);
     }
 
@@ -76,7 +75,7 @@ public class ListingController {
 
 
     @GetMapping("/{listingId}/bookings")
-    public List<BookingDto> getListingBookings(@PathVariable Long listingId) {
+    public List<BookingDto> getListingBookings(@AuthenticationPrincipal UserEntity user, @PathVariable Long listingId) {
         return bookingService.findBookingsByListingId(user.getId(), listingId);
     }
 }
