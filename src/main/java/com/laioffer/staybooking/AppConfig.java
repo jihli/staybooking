0b6c1b4a7150b409a64f1672a7103f75bfaf56d8
jihaoli <a276901052@outlook.com>
2024-12-30 15:15:50 -0800

    12/30 - Google Map API Implemented

---
+++ src/main/java/com/laioffer/staybooking/AppConfig.java
@@ -0,0 +1,50 @@
+package com.laioffer.staybooking;
+
+
+import com.google.auth.Credentials;
+import com.google.auth.oauth2.ServiceAccountCredentials;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+import com.google.maps.GeoApiContext;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+import java.io.IOException;
+
+
+@Configuration
+public class AppConfig {
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests(auth ->
+                        auth
+                                .requestMatchers( "/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        return http.build();
+    }
+
+
+    @Bean
+    public Storage storage() throws IOException {
+        Credentials credentials = ServiceAccountCredentials.fromStream(getClass().getClassLoader().getResourceAsStream("credentials.json"));
+        return StorageOptions.newBuilder().setCredentials(credentials).build().getService();
+    }
+
+
+    @Bean
+    public GeoApiContext geoApiContext(@Value("${staybooking.geocoding.key}") String apiKey) {
+        return new GeoApiContext.Builder().apiKey(apiKey).build();
+    }
+}
